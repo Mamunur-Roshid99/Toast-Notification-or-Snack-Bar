1e669f842ad9edbabb7bd74a73b89903deb36888
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-15 20:03:51 +0600

    snak bar

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const toastBox = document.querySelector("#toastBox");
 
 const successMsg = `<i class="fa-solid fa-circle-check"></i> Successfully submitted`;
-const errorMsg = `<i class="fa-solid fa-circle-xmark"></i> Please fix the error`;
-const invalidMsg = `<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again`;
+const errorMsg = `<i class="fa-solid fa-circle-xmark error"></i> Please fix the error`;
+const invalidMsg = `<i class="fa-solid fa-circle-exclamation invalid"></i> Invalid input, check again`;
 
 function showToast(msg) {
     const toast = document.createElement("div");
     toast.classList.add("toast");
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    setTimeout(() => {
+        toast.remove();
+    }, 5000);
 }
